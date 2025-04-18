9091e610ea2349c5db108873ce20a048e9a87496
Danya <daniilklenin13@gmail.com>
2025-04-18 18:30:33 +0500

    new character+animating+watch_to_mouse, resolution fix

--- main_character/character_body_2d.gd
+++ main_character/character_body_2d.gd
@@ -3,28 +3,33 @@ extends CharacterBody2D
 
 const SPEED = 40.0
 
-
+func watch_toCam():
+	var mouse_pos = get_global_mouse_position()
+	var direction_to_mouse = (mouse_pos - global_position).normalized()
+	$AnimatedSprite2D.rotation = direction_to_mouse.angle()+1.5708
 
 func _physics_process(delta: float) -> void:
 	var speed=SPEED
 	
 	if Input.is_key_pressed(KEY_G):
 		$"../house".position.x+=SPEED*delta
+		
+	watch_toCam()
 	
-	#var mouse_pos = get_viewport().get_mouse_position()
-	#mouse_pos=mouse_pos- Vector2(90,180)
-	#mouse_pos.x=mouse_pos.x/90
-	#mouse_pos.y=mouse_pos.y/180
-	#$Sprite2D
-	#var sin_angle=mouse_pos.x/mouse_pos.y
-	
-	
-	$Sprite2D.rotation+=PI*0.5*delta
 	if Input.is_key_pressed(KEY_SHIFT):
 		speed*=2
 	
-	var direction = Input.get_axis("ui_left", "ui_right")
-	velocity.x=speed  * direction
-	direction = Input.get_axis("ui_up", "ui_down")
-	velocity.y=speed  * direction
+	var direction1 = Input.get_axis("ui_left", "ui_right")
+	
+	velocity.x=speed  * direction1
+	var direction2 = Input.get_axis("ui_up", "ui_down")
+	
+	
+	if direction1!=0 || direction2!=0:
+		$AnimatedSprite2D.play("default")
+	else:
+		$AnimatedSprite2D.stop()
+	
+	velocity.y=speed  * direction2
+	
 	move_and_slide()
